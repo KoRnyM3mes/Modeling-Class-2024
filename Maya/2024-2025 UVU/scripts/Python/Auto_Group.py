d9cbc4c415fc5fe60c07520832a2ef4745241afa
Christopher Tate <CrustRust15@gmail.com>
2024-11-18 12:33:02 -0700

    Clean up code

--- Maya/2024-2025 UVU/scripts/Python/Auto_Group.py
+++ Maya/2024-2025 UVU/scripts/Python/Auto_Group.py
@@ -2,10 +2,10 @@ import maya.cmds as cmd
 
 def Grouper():
     selectedObjs = cmd.ls(selection = True)
-    for i in range(len(selectedObjs)):
-        trans = cmd.xform(selectedObjs[i], q=True, ws=True, t=True)
-        rot = cmd.xform(selectedObjs[i], q=True, ws=True, ro=True)
-        cmd.xform(selectedObjs[i], os=True, t=[0, 0, 0], ro=[0, 0, 0])
-        grps = cmd.group(selectedObjs[i], name = (selectedObjs[i] + "_Grp"))
+    for obj in selectedObjs:
+        trans = cmd.xform(obj, q=True, ws=True, t=True)
+        rot = cmd.xform(obj, q=True, ws=True, ro=True)
+        cmd.xform(obj, os=True, t=[0, 0, 0], ro=[0, 0, 0])
+        grps = cmd.group(obj, name = (obj + "_Grp"))
         cmd.xform(grps, ws=True, t=[trans[0], trans[1], trans[2]], ro=[rot[0], rot[1], rot[2]])
 Grouper()
--- Maya/2024-2025 UVU/scripts/Python/Control_Maker.py
+++ Maya/2024-2025 UVU/scripts/Python/Control_Maker.py
@@ -1,24 +1,24 @@
 import maya.cmds as cmd
 
 def Group(ctrls):
-    for i in range(len(ctrls)):
-        trans = cmd.xform(ctrls[i], q=True, ws=True, t=True)
-        rot = cmd.xform(ctrls[i], q=True, ws=True, ro=True)
-        cmd.xform(ctrls[i], os=True, t=[0, 0, 0], ro=[0, 0, 0])
-        grps = cmd.group(ctrls[i], name = (ctrls[i] + "_Grp"))
+    for ctrl in ctrls:
+        trans = cmd.xform(ctrl, q=True, ws=True, t=True)
+        rot = cmd.xform(ctrl, q=True, ws=True, ro=True)
+        cmd.xform(ctrl, os=True, t=[0, 0, 0], ro=[0, 0, 0])
+        grps = cmd.group(ctrl, name = (ctrl + "_Grp"))
         cmd.xform(grps, ws=True,  t=[trans[0], trans[1], trans[2]], ro=[rot[0], rot[1], rot[2]])   
 
 def ControlMaker():
     selectedObjs = cmd.ls(selection = True)
     ctrlList= []
-    for i in range(len(selectedObjs)):
-        rot = cmd.xform(selectedObjs[i], q=True, ws=True, ro=True)
-        trans = cmd.xform(selectedObjs[i], q=True, ws=True, t=True)
-        if '_' in selectedObjs[i]:
-            nameLs = selectedObjs[i].rpartition('_')
+    for obj in selectedObjs:
+        rot = cmd.xform(obj, q=True, ws=True, ro=True)
+        trans = cmd.xform(obj, q=True, ws=True, t=True)
+        if '_' in obj:
+            nameLs = obj.rpartition('_')
             ctrlName = (nameLs[0] + "_Ctrl")
         else:
-            ctrlName = (selectedObjs[i] + "_Ctrl")
+            ctrlName = (obj + "_Ctrl")
         ctrlList.append(ctrlName)
         ctrl = cmd.circle(name = ctrlName)
         cmd.xform(ctrl, t=[trans[0], trans[1], trans[2]], ro=[rot[0], rot[1], (rot[2] + 90)])
