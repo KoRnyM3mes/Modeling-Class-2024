2a92f38f931d3489cb0832f03290c6a351eb0f76
Christopher Tate <CrustRust15@gmail.com>
2024-12-08 19:37:28 -0700

    added re-namer script

---
+++ Maya/2024-2025 UVU/scripts/Python/Rename.py
@@ -0,0 +1,17 @@
+import maya.cmds as cmd
+
+def Rename(name=str):
+    sels = cmd.ls(selection = True)
+    hashNum = name.count('#')
+    nameParts = name.rpartition('#')
+    namePartsL = list(nameParts)
+    namePartsL[0] = namePartsL[0].replace('#','')
+
+    i = 1
+    for sel in sels:
+      iStr = str(i).zfill(hashNum)
+      i += 1
+      newName = f'{namePartsL[0]}{iStr}{namePartsL[2]}'
+      cmd.rename(sel, newName) 
+
+Rename('leg_###_jnt')
